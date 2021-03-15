0f3ae3ceaec30a20bec74aa9f147cc359d55d479
LadyPerez90 <josefa_groce@yahoo.com>
2021-03-14 20:42:26 -0500

    turning in assignment

---
+++ Groce_Josefa_Algo_Practice_Loops.js
@@ -0,0 +1,13 @@
+//Print Odds 1-20
+
+// for(let i=1; i<20; i+=2){
+//     console.log(i);
+// }
+
+//Sum and Print 1-5
+
+// let sum=0;
+// for(let i=1; i<=5; i++){
+//     sum+=i;
+//     console.log("Num:", i, "Sum:", sum);
+// }
